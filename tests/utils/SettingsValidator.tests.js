3ce8079813067af35378ba0c387ee610cb4d7b14
jankapunkt <jkuester@uni-bremen.de>
2018-06-18 14:51:41 +0200

    Naming refactored

--- tests/utils/SettingsValidator.tests.js
+++ tests/utils/SettingsValidator.tests.js
@@ -11,44 +11,44 @@ describe('SettingsValidator', function () {
 	it ("throws an erorr if the source object is invalid", function(){
 		assert.throws(function(){
 			SettingsValidator.validate(null);
-		}, SettingsValidator.errors.NOT_VALID_OBJECT);
+		}, SettingsValidator.ERRORS.NOT_VALID_OBJECT);
 
 		let source;
 		assert.throws(function(){
 			SettingsValidator.validate(source);
-		}, SettingsValidator.errors.NOT_VALID_OBJECT);
+		}, SettingsValidator.ERRORS.NOT_VALID_OBJECT);
 
 		source = function(){};
 		assert.throws(function(){
 			SettingsValidator.validate(source);
-		}, SettingsValidator.errors.NOT_VALID_OBJECT);
+		}, SettingsValidator.ERRORS.NOT_VALID_OBJECT);
 
 		source = 1;
 		assert.throws(function(){
 			SettingsValidator.validate(source);
-		}, SettingsValidator.errors.NOT_VALID_OBJECT);
+		}, SettingsValidator.ERRORS.NOT_VALID_OBJECT);
 
 		source = "some name";
 		assert.throws(function(){
 			SettingsValidator.validate(source);
-		}, SettingsValidator.errors.NOT_VALID_OBJECT);
+		}, SettingsValidator.ERRORS.NOT_VALID_OBJECT);
 
 		source = true;
 		assert.throws(function(){
 			SettingsValidator.validate(source);
-		}, SettingsValidator.errors.NOT_VALID_OBJECT);
+		}, SettingsValidator.ERRORS.NOT_VALID_OBJECT);
 	});
 
 
 	it ("throws on unsupported element keys", function(){
 		assert.throws(function(){
 			SettingsValidator.validate({unsupported:true});
-		}, SettingsValidator.errors.UNSUPPORTED_KEY);
+		}, SettingsValidator.ERRORS.UNSUPPORTED_KEY);
 	});
 
 	it ("throws on unsupported element values", function(){
 		assert.throws(function(){
 			SettingsValidator.validate({process:1});
-		}, SettingsValidator.errors.UNSUPPORTED_VALUE);
+		}, SettingsValidator.ERRORS.UNSUPPORTED_VALUE);
 	});
 });
